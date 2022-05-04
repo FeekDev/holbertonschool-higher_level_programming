5f732f3400eba2b1f9a2ee3d5fa323a7c27d5a52
felipeholbi <4758@holbertonschool.com>
2022-05-04 00:39:46 -0500

    ascci

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# obtained range
+number_ascii = range(97, 123)
+
+# change to alphabet and iteration
+for i in number_ascii:
+    number_ascii = chr(i)
+    print(number_ascii, end=" ")
