67e25db2892a8426f09244052d33563dc8b920e0
felipeholbi <4758@holbertonschool.com>
2022-05-04 11:54:52 -0500

    6_combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# range an iteration
+for i in range(0, 10):
+# i is the first number
+    for n in range((i + 1), 10):
+# n is the second number
+        if i == 8 and n == 9:
+            print(f"{i}{n}")
+        else:
+            print(f"{i}{n}", end=", ")
