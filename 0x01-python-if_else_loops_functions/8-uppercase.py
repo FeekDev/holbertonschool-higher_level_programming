e1eddf9670ae5ab97f176a718c589f3da10e651c
felipeholbi <4758@holbertonschool.com>
2022-05-04 13:57:32 -0500

    8_uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Prototype
+def uppercase(str):
+    for i in str:
+        c = ord(i)
+        if c <= 122 and c >= 97:
+            print(f"{chr(c - 32)}", end="")
+        else:
+            print(f"{chr(c)}", end="")
+
+    print()
