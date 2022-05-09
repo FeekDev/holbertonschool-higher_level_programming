442ec6ac6ee0e951a5a64627fa4cf3277bf9f0d7
felipeholbi <4758@holbertonschool.com>
2022-05-09 17:52:39 -0500

    0_list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+def print_list_integer(my_list=[]):
+    # allow the list and optional lenght
+    randlist = my_list
+    # this loop travel the list and print
+    for i in randlist:
+        print(f"{i}")
