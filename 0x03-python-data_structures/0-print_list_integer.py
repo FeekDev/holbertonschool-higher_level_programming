c146e395aeccd641dd39a6d9d88f4889b96ebedc
felipeholbi <4758@holbertonschool.com>
2022-05-09 18:04:43 -0500

    0_fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
     # allow the list and optional lenght
     randlist = my_list
     # this loop travel the list and print
     for i in randlist:
         print(f"{i}")
+
+
+if __name__ == "__main__":
+    print_list_integer(my_list=[])
