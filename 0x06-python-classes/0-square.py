e4f55313475e12dd7e5bd2bb478b3f2bf9dc9569
felipeholbi <4758@holbertonschool.com>
2022-05-24 17:10:21 -0500

    0_square_and_README

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """
+    The class is for init create a sqaure
+    ...
+    in this file the class not have, attributes
+    and either methods, only the constructor.
+    """
+    def _init_(self):
+        """
+        Parameters: none
+        ----------
+        only itself.
+        """
+        return
