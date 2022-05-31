aad858fbc3657960aae6e30ae3fe3aba146604ac
felipeholbi <4758@holbertonschool.com>
2022-05-30 23:16:55 -0500

    fix3_test_&_code

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -17,8 +17,3 @@ def say_my_name(first_name, last_name=""):
 
     else:
         print("My name is {} {}".format(first_name, last_name))
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
