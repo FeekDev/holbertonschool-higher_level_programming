5278306dc3047143af871f4aefb67f6bfb57b3d8
felipeholbi <4758@holbertonschool.com>
2022-05-31 21:04:34 -0500

    1_fix_init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,33 +16,33 @@ class Rectangle:
         width: the purpose is set the value
         height: the purpose is set the value
         """
-        self.height = height
         self.width = width
+        self.height = height
 
+    @property
     def width(self):
         """The getter parameters: itself"""
         return self.__width
 
+    @width.setter
     def width(self, value):
         """The setter parameters: value"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-    width = property(width, width)
+        self.__width = value
 
+    @property
     def height(self):
-        return self.__height
         """The getter parameters: itself"""
+        return self.__height
 
+    @height.setter
     def height(self, value):
         """The setter parameters: value"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-    height = property(height, height)
+        self.__height = value
