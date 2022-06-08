0be749a1eeef0168c0de86c03e9549270d59d468
felipeholbi <4758@holbertonschool.com>
2022-06-08 18:23:31 -0500

    1_fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     """
     create the file and write
     """
-    with open(filename, 'w+', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
         wordlist = text.split(', ')
     numb_char = 0
