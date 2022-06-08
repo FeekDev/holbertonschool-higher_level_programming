53783aee237e0db71d7a84725ad2eef259aef14f
felipeholbi <4758@holbertonschool.com>
2022-06-08 18:04:13 -0500

    1_write

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+create the file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    create the file and write
+    """
+    with open(filename, 'w+', encoding="UTF8") as f:
+        f.write(text)
+        wordlist = text.split(', ')
+    numb_char = 0
+    for word in wordlist:
+        for char in word:
+            numb_char += 1
+    return numb_char
