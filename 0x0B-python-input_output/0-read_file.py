6dbf2991d231b3ed55ccf8c3b27cd3daca36c93b
felipeholbi <4758@holbertonschool.com>
2022-06-08 18:13:23 -0500

    0_fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     statment in mode read
     """
-    with open('my_file_0.txt', encoding="UTF8") as f:
+    with open('my_file_0.txt', encoding="utf-8") as f:
         read_file = f.read()
-    print(read_file)
+    print(read_file, end="")
