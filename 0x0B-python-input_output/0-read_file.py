4c7c6ccd8a8af7c965c16a74361459d1814c121d
felipeholbi <4758@holbertonschool.com>
2022-06-08 18:15:47 -0500

    0_fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,9 +5,7 @@ reads a text file and prints it to stdout
 
 
 def read_file(filename=""):
-    """
-    statment in mode read
-    """
-    with open('my_file_0.txt', encoding="utf-8") as f:
+    """statment in mode read"""
+    with open(filename, "r", encoding="utf-8") as f:
         read_file = f.read()
     print(read_file, end="")
