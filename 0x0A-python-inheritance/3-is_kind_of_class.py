e6974e0d95375edef0a15b902be5f77a4bd19dd5
felipeholbi <4758@holbertonschool.com>
2022-06-09 12:44:02 -0500

    fix_3_kind

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -13,5 +13,5 @@ def is_kind_of_class(obj, a_class):
     """
     if type(obj) == a_class:
         return True
-    if issubclass(type(obj), a_class):
+    if isinstance(type(obj), a_class):
         return True
