173581ab2b33db769be3f720991042261480aa55
felipeholbi <4758@holbertonschool.com>
2022-06-09 11:57:34 -0500

    2_same

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module verify the instance
+"""
+def is_same_class(obj, a_class):
+    """
+    Return True if is the same instance 0 otherwise
+    """
+    return type(obj) == a_class
