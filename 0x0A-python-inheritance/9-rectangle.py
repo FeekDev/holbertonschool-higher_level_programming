dc40d187cfe88f10299d2516adbcbaef7042aa95
felipeholbi <4758@holbertonschool.com>
2022-06-10 15:07:21 -0500

    9_rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module subclass BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    the subclass
+    """
+    def __init__(self, width, height):
+        """
+        Constructor
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        find the area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Print the rectanle description
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
