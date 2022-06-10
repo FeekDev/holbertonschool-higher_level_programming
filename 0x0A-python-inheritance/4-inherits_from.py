e91c4e125a8945bfb9bec712cc93376accb9806f
felipeholbi <4758@holbertonschool.com>
2022-06-10 10:41:55 -0500

    fix_4_inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,5 +8,4 @@ def inherits_from(obj, a_class):
     """
      returns True if is - return false otherwise
     """
-    if type(obj) != a_class:
-        return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
