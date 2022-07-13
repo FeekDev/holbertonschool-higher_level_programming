58f6c4f99b563c27cd6f0b4d6dc5e28752d1f1e6
felipeholbi <4758@holbertonschool.com>
2022-07-13 15:05:45 -0500

    4_first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- creates a table called first_table in your MYSQL
+-- will be passed as an and If the table already exists, not fail
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
