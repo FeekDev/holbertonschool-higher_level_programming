d3f619322657e8023d982249b0f1f6127d60418c
felipeholbi <4758@holbertonstudents.com>
2022-08-15 16:46:42 -0500

    1_multi_languajes
    print diferents words to ever programming languaje

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const Cprint = 'C is fun';
+const Print = 'Python is cool';
+const JSprint = 'JavaScript is amazing';
+console.log(Cprint);
+console.log(Print);
+console.log(JSprint);
