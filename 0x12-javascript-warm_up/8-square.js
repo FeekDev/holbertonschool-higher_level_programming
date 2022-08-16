a6c32bb08a7bf3a74c895aadddc05edbe588190e
felipeholbi <4758@holbertonstudents.com>
2022-08-15 23:51:35 -0500

    P8
    print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+let string = '';
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    for (let j = 0; j < process.argv[2]; j++) {
+      string += 'x';
+    }
+    string += '\n';
+  }
+  console.log(string);
+}
