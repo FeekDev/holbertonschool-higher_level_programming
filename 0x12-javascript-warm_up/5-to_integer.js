26ce2e273ec7cf1b5f4d43695b2b456badaf1242
felipeholbi <4758@holbertonstudents.com>
2022-08-15 22:42:03 -0500

    P. 5
    check is is number else casting

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const process = require('process');
+
+if ((isNaN(process.argv[2])) || (process.argv[2] === undefined)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:' + ' ' + parseInt(process.argv[2]));
+}
