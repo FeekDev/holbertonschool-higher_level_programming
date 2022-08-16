88368ff3fc6df6e3a03442de4f7d523b968133a2
felipeholbi <4758@holbertonstudents.com>
2022-08-16 08:35:52 -0500

    fix point 8
    conditional in after second for

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 const process = require('process');
 let string = '';
+let i;
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (i = 0; i < process.argv[2]; i++) {
     for (let j = 0; j < process.argv[2]; j++) {
       string += 'x';
     }
-    string += '\n';
+    if (i < (process.argv[2] - 1)) {
+      string += '\n';
+    }
   }
   console.log(string);
 }
