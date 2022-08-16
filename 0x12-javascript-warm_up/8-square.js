b5e9e3db2d98dd8c4da5bcdd2e787e086228c3e4
felipeholbi <4758@holbertonstudents.com>
2022-08-16 17:47:23 -0500

    fix the loop

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
-let string = '';
-let i;
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (i = 0; i < process.argv[2]; i++) {
-    for (let j = 0; j < process.argv[2]; j++) {
-      string += 'x';
-    }
-    if (i < (process.argv[2] - 1)) {
-      string += '\n';
-    }
+  const string = 'X';
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(string.repeat(process.argv[2]));
   }
-  console.log(string);
 }
