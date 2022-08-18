0428b6227bcf962af66b46eb55fa4742b969af92
felipeholbi <4758@holbertonstudents.com>
2022-08-18 17:27:15 -0500

    point 4
    change the function to arrow function

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,26 +2,19 @@
 const axios = require('axios').default;
 const URL = process.argv[2];
 
-async function getCharacter () {
-  try {
-    const response = await axios.get(URL);
-    const data = response.data.results;
-    const characters = [];
-    for (let i = 0; i < data.length; i++) {
-      characters.push(data[i].characters);
-    }
+axios.get(URL)
+  .then((response) => {
+    const films = response.data['results'];
     let count = 0;
-    for (let j = 0; j < characters.length; j++) {
-      for (let k = 0; k < characters[j].length; k++) {
-        if (characters[j][k].includes('18')) {
-          count++;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes('/18/')) {
+          count += 1;
         }
       }
     }
     console.log(count);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getCharacter();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
