5aee73b2d850aa7f09ba191345d618e03877cefc
felipeholbi <4758@holbertonstudents.com>
2022-08-30 16:55:56 -0500

    point 1
    order the row by ASCCI

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script inser data in db"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                    passwd=argv[2], db=argv[3])
+    cur = db_connection.cursor()
+    cur.execute("""SELECT * FROM states
+                WHERE ASCII(name) = 78
+                ORDER BY states.id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db_connection.close()
