9e983f5a7d30053c6d2750b12aefae782c3f429b
felipeholbi <4758@holbertonstudents.com>
2022-08-31 17:29:42 -0500

    point 3
    create a safe sql

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script inser data in db"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                    passwd=argv[2], db=argv[3])
+    cur = db_connection.cursor()
+    cur.execute("""SELECT *
+                FROM states\
+                WHERE name
+                LIKE %(state)s ORDER BY states.id ASC""", {'state': argv[4]})
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db_connection.close()
