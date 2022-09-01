9496e9054d774b506776b0a5d51972367c4337ee
felipeholbi <4758@holbertonstudents.com>
2022-09-01 14:56:54 -0500

    point 12
    update the id nth position

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -15,9 +15,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
     query = session.query(State).filter(State.id == 2).one()
     query.name = 'New Mexico'
     session.commit()
-
     session.close()
