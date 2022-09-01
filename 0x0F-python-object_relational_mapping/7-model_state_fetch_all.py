e4b1949eff9b3621904a29c70afafcdeaf4b05a2
felipeholbi <4758@holbertonstudents.com>
2022-09-01 11:52:52 -0500

    point 7
    pycodestyle fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,15 +2,15 @@
 """
 this script list all states
 """
-from sys import argv
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                          .format(argv[1], argv[2], argv[3]),
-                          pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
