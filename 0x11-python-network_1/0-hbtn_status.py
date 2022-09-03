8ce0b3ce3b08c148d22b94e9e7b2c35282878c0f
felipeholbi <4758@holbertonstudents.com>
2022-09-03 18:21:55 -0500

    fix point 0
    change the print f to .format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     with urlopen("https://intranet.hbtn.io/status") as response:
         body = response.read()
 
-print("Body response:$")
-print(f"\t- type: {body.__class__}")
-print(f"\t- content: {body}")
-print(f"\t- utf8 content: {body.decode('utf-8')}")
+print("Body response:")
+print("\t- type: {}".format(body.__class__))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
