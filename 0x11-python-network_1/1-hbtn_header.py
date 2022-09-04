65428417279d2e6a5849c6a98909c8ecb81922f4
felipeholbi <4758@holbertonstudents.com>
2022-09-03 19:23:56 -0500

    point 1
    fix the argv and space

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from urllib.request import urlopen
-from sys import argv
+import sys
 '''
 This script make
     ------
 -> sends a request to the URL
 -> before the request obtain the header
 '''
+
 if __name__ == "__main__":
-    with urlopen(argv[1]) as response:
+    with urlopen(sys.argv[1]) as response:
         header = response.getheader('X-Request-Id')
     print(header)
