b18705b044ab9d829d8f8e5969d1025c120415be
felipeholbi <4758@holbertonstudents.com>
2022-09-03 20:45:29 -0500

    fix 3
    add the url before the with

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,10 +7,13 @@ sends a POST request to the passed URL
  the body of the response (decoded in utf-8)
 """
 if __name__ == "__main__":
-    from urllib.request import urlopen, HTTPError, URLError
+    from urllib.request import urlopen, Request
+    from urllib.request import HTTPError, URLError
     from sys import argv
+
+    my_URL = Request(argv[1])
     try:
-        with urlopen(argv[1]) as my_URL:
-            print(my_URL.read().decode('utf-8'))
+        with urlopen(my_URL) as response:
+            print(response.read().decode('utf-8'))
     except HTTPError as e:
-        print('Error code: ', e.code)
+        print('Error code:', e.code)
